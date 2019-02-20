edd698c11e9ac7d4bef6ebaf90bd60cee5fcab26
unknown <D:\\Mail>
2019-02-20 13:52:28 +0200

    правки index4

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
+
+//CUSTOM SCROLL
+(function($){
+	$(window).on("load",function(){
+			$(".recepits__table").mCustomScrollbar();
+			$('select').niceSelect();
+	});
+})(jQuery);
+
+
 let mobileMenu = document.querySelector('.menu-toggle');
 let overlay = document.querySelector('.overlay');
 let mobile = document.querySelector('.mobile');
 let menuToggle = document.querySelector('.menu-toggle');
 let section = document.querySelectorAll('section');
 let logo = document.querySelector('.logo');
-let w8Form = document.querySelector('.w8__form');
-let w8Btn = document.getElementById('w8');
-let w8SaveBtn = document.getElementById('w8-save');
+
 
 
 
@@ -22,31 +30,43 @@ mobileMenu.addEventListener('click', (e) => {
 	logo.classList.toggle('blur');
 });
 
-w8Btn.addEventListener('click', function(e) {
-	e.preventDefault();
-	w8Form.classList.toggle('dn');
-});
 
-w8SaveBtn.addEventListener('click', function(e) {
-	e.preventDefault();
-	let input = document.querySelectorAll('.w8__form input'),
-	inputCheckbox = document.querySelectorAll('.w8__form input[type="radio"]');
-	input.forEach(function(item) {
-		item.value = '';
-	});
-	inputCheckbox.forEach(function(item) {
-		item.checked = false;
-	});
-	w8Form.classList.add('dn');
-});
+(function checkPage () {
+	let w8Form = document.querySelector('.w8__form');
+	let w8Btn = document.getElementById('w8');
+	let w8SaveBtn = document.getElementById('w8-save');
+
+	if (w8Form) {
+		w8Btn.addEventListener('click', function(e) {
+			e.preventDefault();
+			w8Form.classList.toggle('dn');
+		});
+		
+		w8SaveBtn.addEventListener('click', function(e) {
+			e.preventDefault();
+			let input = document.querySelectorAll('.w8__form input'),
+			inputCheckbox = document.querySelectorAll('.w8__form input[type="radio"]');
+			input.forEach(function(item) {
+				item.value = '';
+			});
+			inputCheckbox.forEach(function(item) {
+				item.checked = false;
+			});
+			w8Form.classList.add('dn');
+		});
+	} else {
+		return;
+	}
+
+})();
+
+
+
+ 
 
 
 
 
-//CUSTOM SCROLL
-(function($){
-	$(window).on("load",function(){
-			$(".recepits__table").mCustomScrollbar();
-			$('select').niceSelect();
-	});
-})(jQuery);
+
+
+
