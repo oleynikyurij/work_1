6060d322b1f9fb715f4e7b077568830e675bcb26
unknown <D:\\Mail>
2019-03-18 22:30:13 +0200

    genealogy

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@
 (function ($) {
 	$(window).on("load", function () {
 		$(".recepits__table").mCustomScrollbar();
-			
+
 		$('select').niceSelect();
 	});
-	
+
 
 
 
@@ -68,48 +68,48 @@ mobileMenu.addEventListener('click', (e) => {
 
 	let itemParent = document.querySelectorAll('[data-parent] > .table__item:first-child'),
 		itemParents = document.querySelectorAll('[data-parent]'),
-	 item = document.querySelectorAll('[data-line] > .table__item:first-child'),
-	 itemLine = document.querySelectorAll('[data-line]'),
-	 items = document.querySelectorAll('[data-close]'),
-	 btnClose = document.getElementById('btn-close'),
-	 btnAll = document.getElementById('btn-all');
+		item = document.querySelectorAll('[data-line] > .table__item:first-child'),
+		itemLine = document.querySelectorAll('[data-line]'),
+		items = document.querySelectorAll('[data-close]'),
+		btnClose = document.getElementById('btn-close'),
+		btnAll = document.getElementById('btn-all');
 
 	if (!btnAll) {
 		return;
 	};
 
 
-	btnAll.addEventListener('click', (e)=> {
+	btnAll.addEventListener('click', (e) => {
 		e.preventDefault();
-		itemLine.forEach((i)=> {
+		itemLine.forEach((i) => {
 			i.style.display = 'flex';
 			i.querySelector('.table__item--open').classList.remove('rotate');
 		});
-		items.forEach((i)=> {
+		items.forEach((i) => {
 			i.style.display = 'flex';
-			});
-			itemParents.forEach((i)=> {
-					i.querySelector('.table__item--open').classList.remove('rotate');
-			});	
+		});
+		itemParents.forEach((i) => {
+			i.querySelector('.table__item--open').classList.remove('rotate');
+		});
 	});
 
-	btnClose.addEventListener('click', (e)=> {
+	btnClose.addEventListener('click', (e) => {
 		e.preventDefault();
-		itemLine.forEach((i)=> {
+		itemLine.forEach((i) => {
 			i.style.display = 'none';
 			i.querySelector('.table__item--open').classList.add('rotate');
 		});
-		items.forEach((i)=> {
+		items.forEach((i) => {
 			i.style.display = 'none';
-			});
-
-			itemParents.forEach((i)=> {
-				i.querySelector('.table__item--open').classList.add('rotate');
 		});
-			
+
+		itemParents.forEach((i) => {
+			i.querySelector('.table__item--open').classList.add('rotate');
+		});
+
 	});
 
-	itemParent.forEach(function(elem) {
+	itemParent.forEach(function (elem) {
 		elem.addEventListener('click', function () {
 			// console.log(itemLine);
 			itemLine.forEach((it) => {
@@ -119,19 +119,19 @@ mobileMenu.addEventListener('click', (e) => {
 					it.style.display = 'none';
 				};
 				it.querySelector('.table__item--open').classList.add('rotate');
-				
+
 			});
-					items.forEach((it) => {
-						it.style.display = 'none';
-					});
-				this.querySelector('.table__item--open').classList.toggle('rotate');
+			items.forEach((it) => {
+				it.style.display = 'none';
 			});
+			this.querySelector('.table__item--open').classList.toggle('rotate');
+		});
 	});
-	
+
 
 	item.forEach((el, i) => {
 		el.addEventListener('click', function () {
-		let	items = document.querySelectorAll(`[data-close="${i}-0"]`);
+			let items = document.querySelectorAll(`[data-close="${i}-0"]`);
 			this.querySelector('.table__item--open').classList.toggle('rotate');
 
 			items.forEach((it) => {
@@ -150,13 +150,13 @@ mobileMenu.addEventListener('click', (e) => {
 
 (function () {
 	let filter = document.querySelector('.filter__input');
-	if(!filter) {
+	if (!filter) {
 		return;
 	}
 	let contactOpen = document.querySelectorAll('.contact__open');
 	let infoBlock = document.querySelectorAll('.info');
-	contactOpen.forEach(function(el, i) {
-		el.addEventListener('click', function(e) {
+	contactOpen.forEach(function (el, i) {
+		el.addEventListener('click', function (e) {
 			e.preventDefault();
 			el.classList.toggle('open');
 			infoBlock[i].classList.toggle('active');
@@ -165,11 +165,11 @@ mobileMenu.addEventListener('click', (e) => {
 	});
 })();
 
-(function(){
+(function () {
 
 
 	let range = document.querySelector('#range-block');
-	if(!range) {
+	if (!range) {
 		return;
 	}
 	let circle = document.querySelector('.courved_range .range__circle');
@@ -177,39 +177,40 @@ mobileMenu.addEventListener('click', (e) => {
 	// let section = document.querySelector('.recepits .billing .range');
 
 
-range.addEventListener('click', circlePosition);
-range.addEventListener('mousemove', function (e) {
+	range.addEventListener('click', circlePosition);
+	range.addEventListener('mousemove', function (e) {
 
-	if (event.which == 1) {
-		circlePosition(e);
+		if (event.which == 1) {
+			circlePosition(e);
+		}
+	});
+
+	function circlePosition(e) {
+
+		let x = (e.clientX - range.getBoundingClientRect().left),
+			y;
+
+
+		if (x < 10) {
+			x = 10;
+		} else if (x > 224) {
+			x = 226;
+		}
+
+		circle.style.left = `${x-12}px`;
+		y = Math.round(-41 + 125 * Math.sin(Math.acos((x - 118) / 125)));
+		circle.style.bottom = `${y-22}px`;
+
+
+		let percent = Math.round((x - 10) * 100 / 216);
+		data.innerHTML = `${percent}%`;
 	}
-});
-
-function circlePosition(e) {
-	
-	let x = (e.clientX - range.getBoundingClientRect().left) ,
-		y;
-	
-
-	if (x < 10) {
-		x = 10;
-	} else if (x > 224) {
-		x = 226;
-	}
-
-	circle.style.left = `${x-12}px`;
-	y = Math.round(-41 + 125 * Math.sin(Math.acos((x - 118) / 125)));
-	circle.style.bottom = `${y-22}px`;
-
-	
-	let percent = Math.round((x-10) * 100 / 216);
-	data.innerHTML = `${percent}%`;
-}
 })();
 
-;(function() {
+;
+(function () {
 	let automaile = document.querySelector('.automaile');
-	if(!automaile) {
+	if (!automaile) {
 		return;
 	}
 
@@ -218,24 +219,76 @@ function circlePosition(e) {
 	let infoBlock = document.querySelectorAll('.info');
 	let closeBlock = document.querySelectorAll('.view__close');
 
-	
 
-	viewBtn.forEach(function(el, i) {
-		el.addEventListener('click', function(e) {
+
+	viewBtn.forEach(function (el, i) {
+		el.addEventListener('click', function (e) {
 			e.preventDefault();
 			el.classList.add('view-link');
 			closeBlock[i].classList.remove('hidden');
 			infoBlock[i].classList.add('active');
-			
+
 		});
 	});
 
-	closeBlock.forEach(function(el, i) {
-		el.addEventListener('click', function(e) {
+	closeBlock.forEach(function (el, i) {
+		el.addEventListener('click', function (e) {
 			e.preventDefault();
 			el.classList.toggle('hidden');
 			viewBtn[i].classList.remove('view-link');
 			infoBlock[i].classList.remove('active');
 		});
 	});
+})();
+
+(function () {
+	let genealogy = document.querySelector('.genealogy');
+	if (!genealogy) {
+		return;
+	};
+
+	let itemParent = document.querySelectorAll('[data-parent]'),
+		itemParents = document.querySelectorAll('[data-parent]'),
+		// item = document.querySelectorAll('[data-line] > .table__item:first-child'),
+		item = document.querySelectorAll('[data-line]'),
+		itemLine = document.querySelectorAll('[data-line]'),
+		items = document.querySelectorAll('[data-close]');
+
+
+	itemParent.forEach(function (elem) {
+	
+		elem.addEventListener('click', function () {
+			// console.log(itemLine);
+			itemLine.forEach((it) => {
+				if (it.style.display == 'none') {
+					it.style.display = 'flex';
+				} else {
+					it.style.display = 'none';
+				}
+				it.querySelector('.table__item--open').classList.add('rotate');
+
+			});
+			items.forEach((it) => it.style.display = 'none');
+			this.querySelector('.table__item--open').classList.toggle('rotate');
+		});
+	});
+
+
+	item.forEach((el, i) => {
+		el.addEventListener('click', function () {
+			let items = document.querySelectorAll(`[data-close="${i}-0"]`);
+			this.querySelector('.table__item--open').classList.toggle('rotate');
+
+			items.forEach((it) => {
+				if (it.style.display == 'none') {
+					it.style.display = 'flex';
+				} else {
+					it.style.display = 'none';
+				}
+			});
+		});
+	});
+
+
+
 })();
