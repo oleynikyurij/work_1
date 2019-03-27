d8522fe589a84718fc759cac20dd5c092930151d
unknown <D:\\Mail>
2019-03-27 23:04:01 +0200

    money-dashboard

--- js/script.js
+++ js/script.js
@@ -297,4 +297,26 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	})();
 
+	//Mobile Dashboard
+	;(function() {
+
+		function move() {
+			var elem = document.getElementById("myBar"); 
+			var width = 1;
+			var id = setInterval(frame, 10);
+			function frame() {
+				if (width >= 100) {
+					clearInterval(id);
+				} else {
+					width++; 
+					elem.style.width = width + '%'; 
+				}
+			}
+		}
+
+
+
+
+	})();
+
 });
